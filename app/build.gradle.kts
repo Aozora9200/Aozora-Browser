c561c280211a37ebe13ee412df322cc27df175e7
Hatsukari <aozora9200@gmail.com>
2026-09-21 21:39:33 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.aozora.aozora"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "3.2.0"
+        versionCode = 10
+        versionName = "3.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
